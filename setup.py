34843cb76536f85cc04ea2c46cb36e94c1071903
Jean-Michel Lehker <jlehker@pandora.com>
2019-10-15 12:39:35 -0700

    update python future to 0.18.0

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-
 setup(
     name='pre_commit_hook_futurize',
-    version='0.0.1',
+    version='0.0.2',
     description='Check if your code is python 3 compatible.',
-    install_requires=['future>=0.17.1'],
+    install_requires=['future>=0.18.0'],
 )
+
