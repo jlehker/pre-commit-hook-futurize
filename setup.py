d7ccbd0aa91513d3806fea8101ba73efe4f4533a
Jean-Michel Lehker <jlehker@pandora.com>
2019-10-23 15:51:46 -0700

    bump versions

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='pre_commit_hook_futurize',
-    version='0.0.2',
+    version='0.0.3',
     description='Check if your code is python 3 compatible.',
-    install_requires=['future>=0.18.0'],
+    install_requires=['future>=0.18.1'],
 )
 
